1876d9de817e9dec992b21d68280546dfd939fb8
Nilesh Kolekar <nileshkolekar61@gmail.com>
2025-05-17 12:19:30 +0530

    side item selected on refresh

--- Frontend/Finance-Dashboard/src/app/shared/componets/side-bar/side-bar.component.ts
+++ Frontend/Finance-Dashboard/src/app/shared/componets/side-bar/side-bar.component.ts
@@ -1,27 +1,51 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ConfigService } from '../../services/config.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
-    selector: 'app-side-bar',
-    templateUrl: './side-bar.component.html',
-    styleUrl: './side-bar.component.scss',
-    standalone: false
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.scss',
+  standalone: false
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
 
   isSidebarOpen: boolean = true;
   sideBarList: any;
+  selectedKey: string = '';
 
   @Output() SidebarToggle = new EventEmitter<boolean>;
   @Output() SidebarItem = new EventEmitter<string>;
 
   constructor(
+    private router: Router,
     private readonly configService: ConfigService
-  ) {
+  ) {}
+
+  ngOnInit() {
     this.configService.getConfigFile().subscribe((file: any) => {
       this.sideBarList = Object.entries(file.SideBarItems);
-    })
-   }
+      this.setSelectedKeyFromUrl(this.router.url);
+    });
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.setSelectedKeyFromUrl(event.urlAfterRedirects);
+      });
+  }
+
+  setSelectedKeyFromUrl(url: string) {
+    const segments = url.split('/');
+    const key = segments[2];
+
+    if (key && this.sideBarList.some(([k]: [string, string]) => k === key)) {
+      this.selectedKey = key;
+    } else {
+      this.selectedKey = '';
+    }
+  }
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
@@ -29,6 +53,7 @@ export class SideBarComponent {
   }
 
   selectedListItem(item: string) {
+    this.selectedKey = item;
     this.SidebarItem.emit(item);
   }
 
